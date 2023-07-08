7084af6171fd5011ebaca1a4d3c6ecdcacc3039b
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2023-07-09 01:21:24 +0200

    database acteur en film toevoegen en tonen werkend

---
+++ VideoDatabaseBE/app.py
@@ -0,0 +1,82 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Configuratie van de database 
+# Zorg ervoor dat je de gebruikersnaam, het wachtwoord en de databasenaam invult die je in MAMP hebt ingesteld.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/filmdb'
+
+db = SQLAlchemy(app)
+
+class Acteur(db.Model):
+    __tablename__ = 'acteurs' 
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    voornaam = db.Column(db.String(100))
+    achternaam = db.Column(db.String(100))
+    # Vul hier de rest van je velden in
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+class Film(db.Model):
+    __tablename__ = 'films'
+
+    id = db.Column(db.Integer, primary_key=True)
+    naam = db.Column(db.String(100))
+    bestandsnaam = db.Column(db.String(100))
+    # Voeg hier andere velden toe die je wilt opslaan voor een film
+
+    def __init__(self, naam, bestandsnaam):
+        self.naam = naam
+        self.bestandsnaam = bestandsnaam
+        # Initialiseer andere velden indien nodig
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'naam': self.naam,
+            'bestandsnaam': self.bestandsnaam
+            # Voeg andere velden toe die je wilt opnemen in de dict-representatie
+        }
+
+@app.route('/acteurs', methods=['GET', 'POST'])
+def handle_acteurs():
+    if request.method == 'POST':
+        data = request.json
+        acteur = Acteur(voornaam=data['voornaam'], achternaam=data['achternaam'])  # Vul hier de rest van je velden in
+        db.session.add(acteur)
+        db.session.commit()
+        return {"message": "Acteur succesvol toegevoegd."}, 201
+
+    elif request.method == 'GET':
+        acteurs = Acteur.query.all()
+        return jsonify([acteur.to_dict() for acteur in acteurs])
+
+@app.route('/films', methods=['POST'])
+def create_film():
+    data = request.get_json()
+    naam = data.get('naam')
+    bestandsnaam = data.get('bestandsnaam')
+
+    film = Film(naam=naam, bestandsnaam=bestandsnaam)
+    db.session.add(film)
+    db.session.commit()
+
+    return jsonify({'message': 'Film succesvol toegevoegd'})
+@app.route('/films', methods=['GET'])
+def get_films():
+    films = Film.query.all()
+    film_list = [film.to_dict() for film in films]
+    return jsonify(film_list)
+
+
+@app.route('/video/<bestandsnaam>')
+def speel_video(bestandsnaam):
+    videourl = f"http://localhost:5000/static/videos/{bestandsnaam}"
+    return jsonify({'videourl': videourl})
+    
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
